e93c7bb1e19c36644ee5826d2c57a55ffaa1555b
Богдан Акопян <akopyan.bogdan@gmail.com>
2018-03-06 12:03:12 +0000

    json with jQuery

---
+++ js/jquery-sample.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#page-loaded').html(new Date().toLocaleTimeString());
+    $('#btn-check-account').click(checkAccount);
+    $('#btn-get-fragment').click(getHtml);
+});
+
+function checkAccount() {
+        $.getJSON('client-data.json','',function(result){
+        $('#client-name').html(result['client-name']);
+        $('#account-balance').html(result['account-balance']);
+        }
+    )
+}
+
+function getHtml() {
+    $('#container').load('fragment.html');
+}
